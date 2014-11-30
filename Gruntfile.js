faf30794efe4d24e762bfeb29faee3931405e5ec
yibuyisheng <yibuyisheng@163.com>
2014-11-30 18:37:44 +0800

    add less support

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,49 @@ module.exports = function(grunt) {
                     harmony: true,
                     debug: true,
                     watch: ['server.js', 'templates', 'plugins'],
-                    extensions: ['js,jade']
+                    extensions: ['js,jade'],
+                    forceSync: true
+                }
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["static/less"]
+                },
+                files: {
+                    "static/css/main.css": "static/less/main.less"
+                }
+            }//,
+            // production: {
+            //     options: {
+            //         paths: ["assets/css"],
+            //         cleancss: true,
+            //         modifyVars: {
+            //             imgPath: '"http://mycdn.com/path/to/images"',
+            //             bgColor: 'red'
+            //         }
+            //     },
+            //     files: {
+            //         "path/to/result.css": "path/to/source.less"
+            //     }
+            // }
+        },
+        watch: {
+            less: {
+                files: ['static/less/*.less'],
+                tasks: ['less:development'],
+                options: {
+                    spawn: false
                 }
             }
         }
     });
 
     grunt.loadNpmTasks("grunt-supervisor");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
-    grunt.registerTask('develop', ['supervisor']);
+    grunt.registerTask('develop', ['supervisor', 'watch:less']);
 
 };
