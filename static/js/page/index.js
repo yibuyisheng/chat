09c193873c56e8e003e3f5ff4b69f8fedeb46384
yibuyisheng <yibuyisheng@163.com>
2014-12-02 08:56:42 +0800

    angular

--- static/js/page/index.js
+++ static/js/page/index.js
@@ -1,1 +1,10 @@
-require(['bootstrap', 'angular']);
+require(['bootstrap', 'angular'], function() {
+    angular.module('index', []).controller('IndexController', [
+        '$scope',
+        function($scope) {
+
+        }
+    ]);
+
+    angular.bootstrap(document, ['index']);
+});
