3a70df92e3756a8a04908cf4ef4effdf467c1957
yibuyisheng <yibuyisheng@163.com>
2014-12-07 18:05:07 +0800

    room cache

--- static/js/module/room.js
+++ static/js/module/room.js
@@ -1,13 +1,35 @@
-define(['js/module/module-helper', 'angular'], function(helper) {
+define([
+    'js/module/module-helper',
+    'js/common/utils',
+    'angular'
+], function(
+    helper,
+    utils
+) {
     helper.getModule('moduleModule').factory('roomFactory', [
         '$http',
+        '$q',
         function(
-            $http
+            $http,
+            $q
         ) {
+            var rooms = [];
             function getRoomById(token, roomId) {
-                return $http.get('/get-room-ajax?token=' + token + '&room_id=' + roomId).then(function(result) {
-                    return result.data;
-                });
+                var deferred = $q.defer();
+
+                var room = utils.filter(rooms, function(room) {
+                    return room.id === roomId;
+                })[0];
+                if (room) {
+                    deferred.resolve(room);
+                } else {
+                    $http.get('/get-room-ajax?token=' + token + '&room_id=' + roomId).then(function(result) {
+                        rooms.push(result.data);
+                        deferred.resolve(result.data);
+                    }, deferred.reject);
+                }
+
+                return deferred.promise;
             }
 
             return {
