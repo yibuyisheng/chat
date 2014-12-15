ed7add2a97aeccdbbeeb6a18cd23ed5419ff3ea5
yibuyisheng <yibuyisheng@163.com>
2014-12-15 23:11:16 +0800

    gulp less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function() {
+    gulp.watch(['static/less/**/*.less'], function() {
+        gulp.src('static/less/main.less')
+            .pipe(less({paths: ['static/less']}))
+            .pipe(gulp.dest('static/css'));
+    });
+});
 
 gulp.task('supervisor', function() {
     var childProcess;
@@ -10,13 +20,16 @@ gulp.task('supervisor', function() {
         'plugins/**/*.js',
         'service/**/*.js',
         'templates/**/*.jade'
-    ], function() {
+    ], restart);
+    restart();
+
+    function restart() {
         if (childProcess) {
             process.kill(childProcess.pid, 'SIGHUP');
         }
         childProcess = exec('node --harmony server.js');
-    });
+    }
 });
 gulp.task('develop', function() {
-  // place code for your default task here
+    // place code for your default task here
 });
