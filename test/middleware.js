680be7072f71c9c3b15c046369e6f03522e693ae
yibuyisheng <yibuyisheng@163.com>
2014-12-18 00:40:07 +0800

    genrator function test

--- test/middleware.js
+++ test/middleware.js
@@ -21,4 +21,94 @@ describe('#middleware', function() {
             }
         });
     });
+
+    it.only('generator', function(done) {
+        function * g1 () {
+            yield 1;
+            yield 2;
+            yield 3;
+        }
+
+        /**
+         * g2 相当于：
+         * function * g2 () {
+         *     yield 1;
+         *     yield 2;
+         *     yield 3;
+         * }
+         */
+        function * g2 () {
+            yield * g1();
+        }
+
+        /**
+         * g3()返回一个iterator，当调用iterator.next()的时候，value中的值是g1()返回的iterator
+         */
+        function * g3 () {
+            yield g1();
+            yield 6;
+        }
+
+        /**
+         * g4 相当于：
+         * function * g4 () {
+         *     yield 1;
+         *     yield 2;
+         *     yield 3;
+         *     yield 4;
+         * }
+         */
+        function * g4 () {
+            yield * g1();
+            yield 4;
+        }
+
+        /**
+         * g5 相当于：
+         * function * g5 () {
+         *     yield 5;
+         *     yield 1;
+         *     yield 2;
+         *     yield 3;
+         * }
+         */
+        function * g5 () {
+            yield 5;
+            yield * g1();
+        }
+
+        var toString = Object.prototype.toString;
+
+        var iterator = g1();
+        assert.equal(1, iterator.next().value);
+        assert.equal(2, iterator.next().value);
+        assert.equal(3, iterator.next().value);
+
+        iterator = g2();
+        assert.equal(1, iterator.next().value);
+        assert.equal(2, iterator.next().value);
+        assert.equal(3, iterator.next().value);
+
+        iterator = g3();
+        var g1Iterator = iterator.next().value;
+        assert.equal('[object Generator]', toString.call(g1Iterator));
+        assert.equal(1, g1Iterator.next().value);
+        assert.equal(2, g1Iterator.next().value);
+        assert.equal(3, g1Iterator.next().value);
+        assert.equal(6, iterator.next().value);
+
+        iterator = g4();
+        assert.equal(1, iterator.next().value);
+        assert.equal(2, iterator.next().value);
+        assert.equal(3, iterator.next().value);
+        assert.equal(4, iterator.next().value);
+
+        iterator = g5();
+        assert.equal(5, iterator.next().value);
+        assert.equal(1, iterator.next().value);
+        assert.equal(2, iterator.next().value);
+        assert.equal(3, iterator.next().value);
+
+        done();
+    });
 });
